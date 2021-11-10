fdfd40dbcbfb91fecd2fbfee6b25788cc2e9cdd7
Gohan-117holguin <85535657+Gohan-117holguin@users.noreply.github.com>
2021-11-09 20:55:12 -0500

    Se agrego el admin interface para el cambio de color

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,33 +2,30 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-
+@admin.register(Departamento)
 class DepartamentosAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'tipo_departamento']
     search_fileds = ['nombre']
     list_filter = ['tipo_departamento']
     list_per_page = 10
 
+@admin.register(Salas)
 class SalasAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'tipo_sala']
     search_fileds = ['nombre']
     list_filter = ['tipo_sala']
     list_per_page = 10
 
+@admin.register(Medicos)
 class MedicosAdmin(admin.ModelAdmin):
     list_display = ['nombre', 'especialidad']
     search_fileds = ['nombre']
     list_filter = ['especialidad']
     list_per_page = 10
 
+@admin.register(Paciente)
 class PacientesAdmin(admin.ModelAdmin):
     list_display = ['nombres', 'apellidos', 'tipo_paciente',]
     search_fileds = ['nombre']
     list_filter = ['tipo_paciente']
     list_per_page = 10
-
-
-admin.site.register(Departamento, DepartamentosAdmin)
-admin.site.register(Salas, SalasAdmin)
-admin.site.register(Medicos, MedicosAdmin)
-admin.site.register(Paciente, PacientesAdmin)
